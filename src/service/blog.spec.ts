841e877fd10d1cc750322b0c57afc9b83798e098
therenatus <therinatus@gmail.com>
2023-06-13 14:58:45 +0600

    add delete all users

--- src/service/blog.spec.ts
+++ src/service/blog.spec.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
-import {blogCollection, postCollection} from "../index";
+import {blogCollection, postCollection, userCollection} from "../index";
 
 class Test {
   async deleteAll(req: Request, res: Response) {
     await blogCollection.deleteMany();
     await postCollection.deleteMany();
+    await userCollection.deleteMany();
     res.status(204).send();
 
   }
